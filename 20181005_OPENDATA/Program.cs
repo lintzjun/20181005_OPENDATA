b91266b41f876ac9e08082a1f121e7038635c701
林 子鈞 <linjason19971108@gmail.com>
2018-10-11 22:37:46 +0800

    add 1011

---
+++ 20181005_OPENDATA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _20181005_OPENDATA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
