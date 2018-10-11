6c7e3995a5d6262018c9c2e50f35749cfd29a32f
林 子鈞 <linjason19971108@gmail.com>
2018-10-11 22:39:33 +0800

    add

---
+++ 20181005_OPENDATA/OPENDATA.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _20181005_OPENDATA
+{
+    public class HolidayOpenData
+    {
+        public bool success { get; set; }
+        public Result result { get; set; }
+    }
+    
+    public class Result
+    {
+        public string resource_id { get; set; }
+        public int limit { get; set; }
+        public int total { get; set; }
+        public Field[] fields { get; set; }
+        public Record[] records { get; set; }
+    }
+
+    public class Field
+    {
+        public string type { get; set; }
+        public string id { get; set; }
+    }
+
+    public class Record
+    {
+        public string date { get; set; }
+        public string name { get; set; }
+        public string isHoliday { get; set; }
+        public string holidayCategory { get; set; }
+        public string description { get; set; }
+    }
+
+
+
+
+}
--- 20181005_OPENDATA/Program.cs
+++ 20181005_OPENDATA/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace _20181005_OPENDATA
 {
@@ -6,7 +12,21 @@ namespace _20181005_OPENDATA
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var url = "http://data.ntpc.gov.tw/api/v1/rest/datastore/382000000A-000077-002";
+            var request = WebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "application/json;charset=UTF-8";
+            var response = request.GetResponse() as HttpWebResponse;
+            var responseStream = response.GetResponseStream();
+            var reader = new StreamReader(responseStream, Encoding.GetEncoding("utf-8"));
+            var srcString = reader.ReadToEnd();
+            var jsonData = Newtonsoft.Json.JsonConvert
+                .DeserializeObject<HolidayOpenData>(srcString);
+            foreach (var holiday in jsonData.result.records)
+            {
+                Console.WriteLine($"Date: {holiday.date}, IsHoliday: {holiday.isHoliday}, Category: {holiday.holidayCategory}");
+            }
+            Console.ReadKey();
         }
     }
 }
